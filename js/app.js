28646dc4d37588f6180652925cde0046044e6429
TJRod28 <longputt05@gmail.com>
2026-09-16 11:39:37 -0500

    Lab 12 Checkpoint with JS
    This is a Lab 12 checkpoint with how I was following the instructions listed in the lab for Java Script.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('app.js connected');
+let myFavorite = {
+    name: "Joe's Pizza and Pasta",
+    category: "restaurant",
+    rating: 5,
+    notes: "Great place for custom pizza and filling bread rolls",
+    dateAdded: "September 2026"
+};
+console.log(myFavorite.name);
+let displayText = myFavorite.name + " - Rating: " + myFavorite.rating + "/5";
+
+let today = new Date().toLocaleDateString();
+console.log(today); // 9/16/26
+
+console.log(myFavorite); // click the arrow to expand it
+console.log(typeof myFavorite.name); // string
+console.log(typeof myFavorite.rating); //number
