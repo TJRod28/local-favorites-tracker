6c48ee17807fc326d6a3040336b2b53c3a1c1bb5
TJRod28 <longputt05@gmail.com>
2026-09-18 11:13:13 -0500

    Lab 13 Submission
    Followed Lab 13's submission guides in the lab. Today's lab was rather short in commands thus only needing one commit. Added onto JS about ratings and handles

--- js/app.js
+++ js/app.js
@@ -19,3 +19,20 @@ let placeName = "Joe's Pizza and Pasta";
 let rating = 5;
 console.log(placeName + " - " + rating + "/5");
 console.log("⭐".repeat(rating) + " " + placeName);
+
+function greetFavorite(placeName, rating) {
+    console.log(placeName + " has " + rating + " stars! ");
+}
+greetFavorite("Starbucks", 5); //"Starbucks has 5 stars!"
+
+const nameInput = document.getElementById("name");
+console.log(nameInput.value); //what the user typed
+
+const practiceForm = document.getElementById("add-favorite-form");
+
+function handleSubmit(event) {
+    event.preventDefault(); //stop the page reload
+    console.log("You typed: " + nameInput.value);
+}
+
+practiceForm.addEventListener("submit", handleSubmit);
